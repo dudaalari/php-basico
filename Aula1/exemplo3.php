80ee2fc497d4bfc3abda6ae4cfba33bc1ed72751
Larissa Silva <larissa.e.silva11@aluno.senai.br>
2025-10-10 16:33:43 -0300

    commit

---
+++ Aula1/exemplo3.php
@@ -0,0 +1,11 @@
+<?php
+echo          "testando espaço em branco <br>";
+echo  "testando espaço <br>";
+
+
+//O caracter &nbsp adiciona esoaço em branco
+echo "&#160; &nbsp;        Testando todos espaços em brancos<br>";
+echo    
+"Quebra de linha"
+;
+?>
